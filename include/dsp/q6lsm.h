3ff578aca42af79879e0e8e25204a179ced027c6
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-05-23 11:59:42 +0200

    q6core: Add support for legacy Q6 service versions
    Instead of throwing an error everytime and falling back to old
    service initialization and handling, in case the DSP firmware
    does NOT return the services versions, suppose that we are
    dealing with legacy DSP firmwares on legacy SoCs and also that
    the all the supported legacy SoCs have got V1 service versions.
    
    This is true and usable for a number of legacy SoCs, like
    MSM8956/76, 8996, 8998, SDM630/636/660.
    
    Change-Id: I6eec3244985e0ea891c98c36e76f8a53d803b750
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- include/dsp/q6lsm.h
+++ include/dsp/q6lsm.h
@@ -22,6 +22,8 @@
 
 #define MAX_LSM_SESSIONS 8
 
+#define LSM_API_VERSION_V2 2
+
 typedef void (*lsm_app_cb)(uint32_t opcode, uint32_t token,
 		       uint32_t *payload, uint16_t client_size, void *priv);
 
